e6d7bc120489f4538d4b36bc9f5cf0be165865b4
Grace Kostanich <fl5812597@gmail.com>
2022-07-29 13:27:20 -0700

    Create list of stylists from database | Return stylist list to view

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -15,5 +15,11 @@ namespace HairSalon.Controllers
     {
       _db = db;
     }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
   }
 } 
